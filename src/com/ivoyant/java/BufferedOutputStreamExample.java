e04983511c573c9c8a9ca3c11a72845507a2078b
AmshuPatel <amshup@ivoyant.com>
2025-07-26 09:40:02 +0530

    Buffered File Opearations on binary data

---
+++ src/com/ivoyant/java/BufferedOutputStreamExample.java
@@ -0,0 +1,5 @@
+package com.ivoyant.java;
+
+public class BufferedOutputStreamExample {
+
+}
---
+++ src/com/ivoyant/java/FileReaderExample.java
@@ -0,0 +1,7 @@
+package com.ivoyant.java;
+
+import java.io.FileReader;
+
+public class FileReaderExample {
+    FileReader fr=
+}
