6c4bf034288a9b84f87840cd1fd231b3b88f2859
AmshuPatel <amshup@ivoyant.com>
2025-08-02 22:56:13 +0530

    Add JDBC examples

---
+++ src/com/ivoyant/java/JDBCExample/InsertStudent.java
@@ -0,0 +1,21 @@
+package com.ivoyant.java.JDBCExample;
+import java.sql.*;
+public class InsertStudent {
+
+    public static void main(String[] args) {
+        String jdbc_url = "jdbc:mysql://localhost:3306/studentdb";
+        String username = "root";
+        String password = "root";
+        try{
+            Connection conn = DriverManager.getConnection(jdbc_url, username, password);
+            PreparedStatement psmt=conn.prepareStatement("INSERT INTO students(id,name,grade)VALUES(?,?,?)");
+            psmt.setInt(1,4);
+            psmt.setString(2,"Aliya");
+            psmt.setString(3,"A+");
+            psmt.executeUpdate();
+            System.out.println("Done with insertion");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
